1c935a42669edc0532b527bd3d1771d71773753c
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-03-16 12:54:54 +0100

    Fibonacci

--- src/main/java/ud05arrays/Ud5Ejercicio2.java
+++ src/main/java/ud05arrays/Ud5Ejercicio2.java
@@ -37,8 +37,13 @@ public class Ud5Ejercicio2 {
 	 * @return el array con todos los terminos de fibonacci calculados para cada posicion
 	 */
 	public static long [] fibonacci (int terminos) {;
+		long[] retorno = new long[terminos];
 		
-		return null;
+		for (int i = 0; i < retorno.length; i++) {
+			retorno[i] = fibo(i);
+		}
+		
+		return retorno;
 	}
 	
 	/**
@@ -53,8 +58,14 @@ public class Ud5Ejercicio2 {
 	 * @return valor del termino en la sucesion de Fibonacci
 	 */
 	public static long fibo(int enesimo) {
+		long retorno = 0;
 		
-		return -1;
+		if (enesimo == 1) {
+			retorno = 1;
+		} else if (enesimo > 1) {
+			retorno = fibo(enesimo - 2) + fibo(enesimo - 1);
+		}
+		return retorno;
 	}
 
 }
